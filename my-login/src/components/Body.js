61432ab5c801b92e8c154085f02699be34d4f25b
Pavithranait <pavithrankait@gmail.com>
2023-03-07 15:40:15 +0530

    body page created

---
+++ my-login/src/components/Body.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+
+const Body = () => {
+  return (
+    <div>
+      <SignIn/>
+      <SignUp />
+    </div>
+  );
+};
+
+export default Body;
