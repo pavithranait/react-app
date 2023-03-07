366152d7bf2e51255baeca0be3511ccf366dade4
Pavithranait <pavithrankait@gmail.com>
2023-03-07 15:40:27 +0530

    header created

---
+++ my-login/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Header = () => {
+  return (
+    <div className="header">
+        <h1>Dashboard</h1>
+        <h2>SignIn</h2>
+        <h2>SignIn</h2>
+    </div>
+  );
+};
+
+export default Header;
