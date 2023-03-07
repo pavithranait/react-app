fca0fd94124aa296e24ed5b3f4b1f5e2e63d9e5c
Pavithranait <pavithrankait@gmail.com>
2023-03-07 15:40:42 +0530

    changes made

--- my-login/src/App.js
+++ my-login/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-    <SignIn/>
-    <SignUp />
+    <Home />
     </div>
   );
 }
